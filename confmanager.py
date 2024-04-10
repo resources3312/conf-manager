a00a358283f6186f51b9a75bde96f7e45ac95cc7
ViCoder <x.daahome@gmail.com>
2024-04-10 19:04:13 +0300

    Development....

--- confmanager.py
+++ confmanager.py
@@ -6,3 +6,32 @@
 # Github: 
 # Coded by: Vicoder32
 ######################################
+from art import text2art
+import os
+import sys
+from termcolor import colored
+
+def check_root():
+    if os.getuid() != 0:
+        sys.exit("Running util for root")
+    
+    else:
+        pass
+
+
+
+
+
+def menu():
+    title = colored(text2art('conf manager'), 'yellow')
+    desc = colored("System administration utility", "yellow")
+    co = colored("Choose options:", 'yellow')
+    f = colored("Choose options:", 'green')
+    
+    
+
+def main():
+    check_root()
+
+if __name__ == '__main__':
+    main()
