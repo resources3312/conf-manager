164ab5edd0b896c3385fb9446a292b48beb2f138
ViCoder <x.daahome@gmail.com>
2024-04-10 14:30:41 +0300

    Add main file

---
+++ confmanager.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+######################################
+# This is main file confmanager,
+# In this file only cli interface,
+# any modules in directory
+# Github: 
+# Coded by: Vicoder32
+######################################
